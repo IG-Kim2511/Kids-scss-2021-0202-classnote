c48c13e6ff6586393ed80eee8f2a779a8fc3cb82
iikim <iikim2511@gmail.com>
2021-02-03 15:25:15 -0500

    section3

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 // js 045, learn more btn click, and the circle expand and show guide
 // array.forEach((i)=>{})  : i 파라미터는 array안 하나하나의 데이터
 
-document.querySelectorAll('.card-btn').forEach((btn)=>{
+document.querySelectorAll('.card-btn').forEach((i)=>{
 
-    btn.addEventListener('click',()=>{
-        btn.parentElement.classList.toggle('change');
+    i.addEventListener('click',()=>{
+        i.parentElement.classList.toggle('change');
 
     })
-
 })
+    
